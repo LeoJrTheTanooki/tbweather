50bcbab3fb40505e3682404edc1bd9c562e8b9a8
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-03-29 23:55:35 -0700

    Preparations to upload

--- src/DataServices/DataService.tsx
+++ src/DataServices/DataService.tsx
@@ -1,16 +1,20 @@
+// require('dotenv').config()
 import { Iweather } from "../Interfaces/Interfaces";
 import { IForecast } from "../Interfaces/Interfaces";
+// import { apiKey } from "./environment";
+// import { apiKey } from "./environment";
+const apiKey = process.env.OPEN_WEATHER_API_KEY; 
 
-export const getWeather = async (location: string = 'stockton', apiKey: string = '524901&appid=acfbb5d1a1b814a2931225281e2d44da') => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&id=${apiKey}&units=imperial`);
+export const getWeather = async (location: string = 'stockton') => {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`);
   const data: Iweather = await response.json();
   // console.log(`http://api.openweathermap.org/data/2.5/weather?q=${location}&id=${apiKey}`)
   // console.log(data)
   return data;
 };
 
-export const getForecast = async (location: string = 'stockton', apiKey: string = '524901&appid=acfbb5d1a1b814a2931225281e2d44da') => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&id=${apiKey}&units=imperial`);
+export const getForecast = async (location: string = 'stockton') => {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=imperial`);
   const data: IForecast = await response.json();
   // console.log(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&id=${apiKey}`)
   // console.log(data)
